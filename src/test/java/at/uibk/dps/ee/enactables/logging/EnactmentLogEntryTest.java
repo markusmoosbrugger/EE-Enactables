4ff0c670c7342cd508a1ee687a2bfc715f43e10c
Markus Moosbrugger <markus.l.moosbrugger@student.uibk.ac.at>
2021-04-08 09:53:10 +0200

    Adapted test for enactment log entry

--- src/test/java/at/uibk/dps/ee/enactables/logging/EnactmentLogEntryTest.java
+++ src/test/java/at/uibk/dps/ee/enactables/logging/EnactmentLogEntryTest.java
@@ -6,35 +6,46 @@ import java.time.Instant;
 
 import static org.junit.Assert.*;
 
-public class EnactmentEntryTest {
+public class EnactmentLogEntryTest {
 
   @Test
   public void test() {
     String id = "id1";
     String type = "type1";
     double executionTime = 110.1;
+    double inputComplexity = 0;
+    boolean success = false;
     Instant timestamp = Instant.now();
 
-    EnactmentLogEntry entry = new EnactmentLogEntry(timestamp, id, type, executionTime, false, 0);
+    EnactmentLogEntry entry =
+        new EnactmentLogEntry(timestamp, id, type, executionTime, success, inputComplexity);
     assertEquals(id, entry.getFunctionId());
     assertEquals(type, entry.getFunctionType());
     assertEquals(executionTime, entry.getExecutionTime(), 0.001);
     assertNotNull(entry.getTimestamp());
     assertEquals(0.0, entry.getInputComplexity(), 0.001);
-    assertFalse(entry.isSuccess());
+    assertEquals(success, entry.isSuccess());
 
-    boolean success = true;
-    double inputComplexity = 0.8;
+    String id2 = "id2";
+    String type2 = "type2";
+    double executionTime2 = 90.01;
+    Instant timestamp2 = Instant.now();
+    boolean success2 = true;
+    double inputComplexity2 = 0.8;
 
-    EnactmentLogEntry entryComplete =
-        new EnactmentLogEntry(timestamp, id, type, executionTime, success, inputComplexity);
-    assertEquals(timestamp, entryComplete.getTimestamp());
-    assertEquals(id, entryComplete.getFunctionId());
-    assertEquals(type, entryComplete.getFunctionType());
-    assertEquals(executionTime, entryComplete.getExecutionTime(), 0.001);
-    assertNotNull(entry.getTimestamp());
-    assertEquals(inputComplexity, entryComplete.getInputComplexity(), 0.001);
-    assertEquals(success, entryComplete.isSuccess());
+    entry.setFunctionId(id2);
+    entry.setFunctionType(type2);
+    entry.setExecutionTime(executionTime2);
+    entry.setTimestamp(timestamp2);
+    entry.setSuccess(success2);
+    entry.setInputComplexity(inputComplexity2);
+
+    assertEquals(timestamp2, entry.getTimestamp());
+    assertEquals(id2, entry.getFunctionId());
+    assertEquals(type2, entry.getFunctionType());
+    assertEquals(executionTime2, entry.getExecutionTime(), 0.001);
+    assertEquals(inputComplexity2, entry.getInputComplexity(), 0.001);
+    assertEquals(success2, entry.isSuccess());
   }
 
   @Test
