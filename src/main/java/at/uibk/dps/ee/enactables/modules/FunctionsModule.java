fe5fdc9c86a6a01912a308764508294fde8c0908
Markus Moosbrugger <markus.l.moosbrugger@student.uibk.ac.at>
2021-04-08 09:34:35 +0200

    Resolved too large comment in FunctionsModule

--- src/main/java/at/uibk/dps/ee/enactables/modules/FunctionsModule.java
+++ src/main/java/at/uibk/dps/ee/enactables/modules/FunctionsModule.java
@@ -3,7 +3,8 @@ package at.uibk.dps.ee.enactables.modules;
 import at.uibk.dps.ee.guice.modules.FunctionModule;
 
 /**
- * The {@link FunctionsModule} defines the dynamic aspects of the configuration of functions.
+ * The {@link FunctionsModule} defines the dynamic aspects of the configuration
+ * of functions.
  *
  * @author Fedor Smirnov
  */
