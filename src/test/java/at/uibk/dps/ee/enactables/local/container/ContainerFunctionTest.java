c14855e36eca9446393a62e0e2b471a254545c05
Fedor <fedor@dps.uibk.ac.at>
2021-06-08 07:48:03 +0200

    tests

---
+++ src/test/java/at/uibk/dps/ee/enactables/local/container/ContainerFunctionTest.java
@@ -0,0 +1,41 @@
+package at.uibk.dps.ee.enactables.local.container;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.google.gson.JsonObject;
+import at.uibk.dps.ee.core.ContainerManager;
+import at.uibk.dps.ee.core.exception.StopException;
+import at.uibk.dps.ee.enactables.EnactmentMode;
+import static org.mockito.Mockito.mock;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.verify;
+
+public class ContainerFunctionTest {
+
+  @Test
+  public void test() {
+    String typeId = "funcType";
+    String implId = "localComp";
+    Set<SimpleEntry<String, String>> additional = new HashSet<>();
+    ContainerManager mockManager = mock(ContainerManager.class);
+    String imageName = "image";
+    ContainerFunction tested =
+        new ContainerFunction(typeId, implId, additional, mockManager, imageName);
+    // getters
+    assertEquals(typeId, tested.getTypeId());
+    assertEquals(implId, tested.getImplementationId());
+    assertEquals(EnactmentMode.Local.name(), tested.getEnactmentMode());
+    assertEquals(additional, tested.getAdditionalAttributes());
+    // running the function
+    JsonObject input = new JsonObject();
+    try {
+      tested.processInput(input);
+    } catch (StopException e) {
+      fail();
+    }
+    verify(mockManager).runImage(imageName, input);
+  }
+}
---
+++ src/test/java/at/uibk/dps/ee/enactables/local/container/FunctionFactoryLocalTest.java
@@ -0,0 +1,35 @@
+package at.uibk.dps.ee.enactables.local.container;
+
+import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import at.uibk.dps.ee.core.ContainerManager;
+import at.uibk.dps.ee.core.enactable.FunctionDecoratorFactory;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUser;
+import at.uibk.dps.ee.model.properties.PropertyServiceMappingLocal;
+import net.sf.opendse.model.Mapping;
+import net.sf.opendse.model.Resource;
+import net.sf.opendse.model.Task;
+import static org.mockito.Mockito.mock;
+
+public class FunctionFactoryLocalTest {
+
+  @Test
+  public void test() {
+    Set<FunctionDecoratorFactory> decorators = new HashSet<>();
+    ContainerManager mockManager = mock(ContainerManager.class);
+    FunctionFactoryLocal tested = new FunctionFactoryLocal(decorators, mockManager);
+
+    Task task = PropertyServiceFunctionUser.createUserTask("task", "addition");
+    Resource res = new Resource("r");
+    Mapping<Task, Resource> map =
+        PropertyServiceMappingLocal.createMappingLocal(task, res, "image");
+
+    ContainerFunction result = tested.getContainerFunction(map);
+
+    assertEquals("addition", result.getTypeId());
+    assertEquals(at.uibk.dps.ee.enactables.EnactmentMode.Local.name(), result.getEnactmentMode());
+    assertTrue(result.getAdditionalAttributes().isEmpty());
+  }
+}
